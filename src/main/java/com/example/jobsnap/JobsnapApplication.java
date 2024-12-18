518b1e032e04d335912d2f673685e3d8fe386def
Ana-Maria Suditu <anamaria_suditu@yahoo.com>
2024-12-18 22:59:57 +0200

    Structura proiect

---
+++ src/main/java/com/example/jobsnap/JobsnapApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobsnap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsnapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsnapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jobsnap/JobsnapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobsnap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsnapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
