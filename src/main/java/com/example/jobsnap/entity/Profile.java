efd1706e28ed0e491fd1293d2831aebdfb6f1cb3
Ana-Maria Suditu <anamaria_suditu@yahoo.com>
2025-01-10 17:20:13 +0200

    added profile page for logged users

--- src/main/java/com/example/jobsnap/entity/Profile.java
+++ src/main/java/com/example/jobsnap/entity/Profile.java
@@ -6,7 +6,8 @@ import jakarta.persistence.*;
 public class Profile {
 
     @Id
-    private int id; // Same as Student/Employer ID
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // Optional if you want auto-generation of id
+    private int id; // This will be the same as User's ID
 
     private String firstName;
     private String lastName;
@@ -17,36 +18,76 @@ public class Profile {
     @JoinColumn(name = "cv_id") // Foreign key to the CV table
     private CV cv;
 
-    @ManyToOne
-    //@MapsId // Maps this ID to the Student or Employer's ID
-    @JoinColumn(name = "user_id") // Foreign key to Student/Employer
+    @OneToOne
+    @MapsId  // This will map Profile's id to User's id
+    @JoinColumn(name = "user_id") // Foreign key to User
     private User user; // This could be either Student or Employer
 
     public Profile() {
     }
 
-    public Profile(int id, String firstName, String lastName, String bio, String contactInfo) {
-        this.id = id;
+    public Profile(String firstName, String lastName, String bio, String contactInfo) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.bio = bio;
         this.contactInfo = contactInfo;
     }
 
-
     // Getters and setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-    public String getFirstName() { return firstName; }
-    public void setFirstName(String firstName) { this.firstName = firstName; }
-    public String getLastName() { return lastName; }
-    public void setLastName(String lastName) { this.lastName = lastName; }
-    public String getBio() { return bio; }
-    public void setBio(String bio) { this.bio = bio; }
-    public String getContactInfo() { return contactInfo; }
-    public void setContactInfo(String contactInfo) { this.contactInfo = contactInfo; }
-    public CV getCv() { return cv; }
-    public void setCv(CV cv) { this.cv = cv; }
-    public User getUser() { return user; }
-    public void setUser(User user) { this.user = user; }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public CV getCv() {
+        return cv;
+    }
+
+    public void setCv(CV cv) {
+        this.cv = cv;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
+
