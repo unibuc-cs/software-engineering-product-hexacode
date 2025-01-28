9f21e33c8e2d5711c7b293affaca2d119b107ce0
Ana-Maria Suditu <anamaria_suditu@yahoo.com>
2025-01-28 12:01:39 +0200

    fixed logout

--- frontend/jobsnap/src/pages/Profile.js
+++ frontend/jobsnap/src/pages/Profile.js
@@ -303,8 +303,11 @@ const Profile = () => {
             {user.role === 'student' && (
                 <div className="cv-section">
                     <p className="cv-message">Aici poți vizualiza și gestiona CV-urile create.</p>
-                    <button onClick={handleUploadRedirect} className="button upload-page">
-                        Upload Page
+                    <button
+                        className="bg-blue-700 text-white font-semibold py-2 px-6 rounded-lg shadow-md hover:bg-blue-800 transition-all duration-300 transform hover:scale-105 focus:ring-2 focus:ring-blue-300"
+                        onClick={handleUploadRedirect}
+                    >
+                        View All CVs
                     </button>
 
                     <div className="cv-list">
